506914592c524a8d82e0ea75f6e9bce5f2d285a4
mudra07 <mudra2006@eng.rizvi.edu.in>
2025-10-17 08:21:59 +0530

    Create Exp 1 (a).c

---
+++ Exp 1 (a).c
@@ -0,0 +1,12 @@
+/*Name : Mudra Jadhav
+UIN : 251C016
+Class : civil(fy)
+Div : B*/
+#include<stdio.h>
+Int main( ){
+Printf("Name: \t Mudra Jadhav\n");
+Printf("Age: \t 18\n");
+Printf("Class: \t Civil (fy)\n");
+Printf("UIN: \t 251C016\n");
+Return 0;
+}

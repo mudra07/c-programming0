c2052bfd7841084c8356bc8a202234012837a4fe
mudra07 <mudra2006@eng.rizvi.edu.in>
2025-10-17 08:35:22 +0530

    Create Exp 2 (a).c

---
+++ Exp 2 (a).c
@@ -0,0 +1,21 @@
+/*Name : Mudra Jadhav
+UIN : 251C016
+Class : civil(fy)
+Div : B*/
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Enter an integer: ");
+
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+    printf("%d is an even number.\n", number);
+    } else {
+    printf("%d is an odd number.\n", number);
+    }
+
+    return 0;
+}

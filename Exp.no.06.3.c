d224397c08f3685f6708c9daff381710bf68e41f
mudra07 <mudra2006@eng.rizvi.edu.in>
2025-11-30 17:15:15 +0530

    Create Exp.no.06.3.c

---
+++ Exp.no.06.3.c
@@ -0,0 +1,15 @@
+/*Name : Mudra Jadhav
+UIN : 251C016
+Class : civil(fy)
+Div : B*/
+#include<stdio.h>
+int storage_class()
+{
+int abc =0;
+printf("counter is %d \n", abc);
+}
+int main ()
+{
+storage_class();
+return 0;
+}
